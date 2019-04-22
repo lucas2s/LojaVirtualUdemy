c4b17d5dd7b6eef65c2d27ba0e883096eb6c3645
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-04-21 21:09:49 -0300

    Criação do Projeto

---
+++ src/main/java/com/sartorilucas/lojavirtualudemy/LojaVirtualUdemyApplication.java
@@ -0,0 +1,14 @@
+package com.sartorilucas.lojavirtualudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualUdemyApplication.class, args);
+		
+	}
+
+}
