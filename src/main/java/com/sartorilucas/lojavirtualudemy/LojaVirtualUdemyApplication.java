6ec64a80bc4a3e2848c17f0caf2186cc6d4f5234
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-04-23 01:21:32 -0300

    Criando operação de inserção de categoria

--- src/main/java/com/sartorilucas/lojavirtualudemy/LojaVirtualUdemyApplication.java
+++ src/main/java/com/sartorilucas/lojavirtualudemy/LojaVirtualUdemyApplication.java
@@ -1,13 +1,32 @@
 package com.sartorilucas.lojavirtualudemy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sartorilucas.lojavirtualudemy.domain.Categoria;
+import com.sartorilucas.lojavirtualudemy.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaVirtualUdemyApplication {
+public class LojaVirtualUdemyApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualUdemyApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria (null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 	}
 
