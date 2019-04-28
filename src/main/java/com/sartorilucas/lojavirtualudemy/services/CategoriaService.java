b263d17ea003d6b73b57001c9f5d46024f387d5c
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-04-28 13:39:53 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/sartorilucas/lojavirtualudemy/services/CategoriaService.java
+++ src/main/java/com/sartorilucas/lojavirtualudemy/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.sartorilucas.lojavirtualudemy.domain.Categoria;
 import com.sartorilucas.lojavirtualudemy.repositories.CategoriaRepository;
+import com.sartorilucas.lojavirtualudemy.services.excepitions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional <Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
---
+++ src/main/java/com/sartorilucas/lojavirtualudemy/services/excepitions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.sartorilucas.lojavirtualudemy.services.excepitions;
+
+public class ObjectNotFoundException extends RuntimeException{
+	         
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException (String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
