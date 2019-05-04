b0219f1f3e2c2a0321665afcde68a38e3588dc9b
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-05-04 10:57:44 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/sartorilucas/lojavirtualudemy/resources/CategoriaResource.java
+++ src/main/java/com/sartorilucas/lojavirtualudemy/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.sartorilucas.lojavirtualudemy.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sartorilucas.lojavirtualudemy.domain.Categoria;
+import com.sartorilucas.lojavirtualudemy.dto.CategoriaDTO;
 import com.sartorilucas.lojavirtualudemy.services.CategoriaService;
 
 @RestController
@@ -45,6 +48,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return  ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List <CategoriaDTO>> list() {
+		List <Categoria> list = service.findAll();
+		List <CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	
