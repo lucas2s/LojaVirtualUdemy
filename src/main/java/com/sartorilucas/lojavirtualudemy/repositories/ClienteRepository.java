f15ab21bb266c14acaf87536a5ba8bf5cfc5d599
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-05-22 01:09:13 -0300

    Validação Customizada email nao repetido na Inserção de Cliente

--- src/main/java/com/sartorilucas/lojavirtualudemy/repositories/ClienteRepository.java
+++ src/main/java/com/sartorilucas/lojavirtualudemy/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.sartorilucas.lojavirtualudemy.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sartorilucas.lojavirtualudemy.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository <Cliente, Integer>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/sartorilucas/lojavirtualudemy/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/sartorilucas/lojavirtualudemy/services/validation/ClienteInsertValidator.java
@@ -6,19 +6,28 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sartorilucas.lojavirtualudemy.domain.Cliente;
 import com.sartorilucas.lojavirtualudemy.domain.enuns.TipoCliente;
 import com.sartorilucas.lojavirtualudemy.dto.ClienteNewDTO;
+import com.sartorilucas.lojavirtualudemy.repositories.ClienteRepository;
 import com.sartorilucas.lojavirtualudemy.resources.excepitons.FieldMessage;
 import com.sartorilucas.lojavirtualudemy.services.validation.utils.BR; 
  
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> { 
- 
+	
+	@Autowired
+	private ClienteRepository repo;
+	
     @Override
     public void initialize(ClienteInsert ann) {}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) { 
         List<FieldMessage> list = new ArrayList<>();
+        
+        
         
         if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
         	list.add(new FieldMessage("CpfOuCnpj", "CPF Inválido"));
@@ -27,6 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
         	list.add(new FieldMessage("CpfOuCnpj", "CNPJ Inválido"));
         }
+        
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null) {
+        	list.add(new FieldMessage("Email", "Email já existente!"));
+        }
+        
 		
 		// inclua os testes aqui, inserindo erros na lista               
 		for (FieldMessage e : list) {
