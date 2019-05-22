87f7cacb12e9dfc4895527a86caeb99b9e628cb5
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-05-22 00:21:07 -0300

    Correção POST Clientes

--- src/main/java/com/sartorilucas/lojavirtualudemy/services/ClienteService.java
+++ src/main/java/com/sartorilucas/lojavirtualudemy/services/ClienteService.java
@@ -17,7 +17,6 @@ import com.sartorilucas.lojavirtualudemy.domain.Endereco;
 import com.sartorilucas.lojavirtualudemy.domain.enuns.TipoCliente;
 import com.sartorilucas.lojavirtualudemy.dto.ClienteDTO;
 import com.sartorilucas.lojavirtualudemy.dto.ClienteNewDTO;
-import com.sartorilucas.lojavirtualudemy.repositories.CidadeRepository;
 import com.sartorilucas.lojavirtualudemy.repositories.ClienteRepository;
 import com.sartorilucas.lojavirtualudemy.repositories.EnderecoRepository;
 import com.sartorilucas.lojavirtualudemy.services.excepitions.DataIntegrityExcepition;
@@ -28,9 +27,6 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
-	
-	@Autowired
-	private CidadeRepository cidadeRepository;
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
@@ -84,7 +80,7 @@ public class ClienteService {
 	
 	public Cliente fromDTO (ClienteNewDTO objDto) {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj() ,TipoCliente.toEnum(objDto.getTipo()));
-		Cidade cid = cidadeRepository.getOne(objDto.getCidadeId());
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		Endereco end = new Endereco (null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
